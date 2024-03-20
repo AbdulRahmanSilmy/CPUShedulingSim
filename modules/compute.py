fe263ad6abd8193683bf66c6b0d0f2d906e8ec2f
Abdul Rahman Silmy <abdul.rahman.silmy@gmail.com>
2024-03-19 18:58:55 -0700

    added FCFS

--- modules/compute.py
+++ modules/compute.py
@@ -1,6 +1,42 @@
 #This module contains the 
 import numpy as np 
 
+def _FCFS(task_info:dict) -> np.ndarray:
+    """
+    Computes the scheduling base on the first come first serve
+    
+    Parameters
+    ----------
+
+    task_info: dict 
+        A dictionary containing both the scheduling algorithm and task details
+
+        The dictionary should contain the following key strings and value types
+        presented below:
+
+        {"arrival_time": np.array 1d,
+         "wc_exec_time": np.array 1d}
+
+    computed_results: np.array
+        A 2d numpy array containing the computed results.
+
+    """
+
+    arrival_time=task_info['arrival_time']
+    wx_exec_time=task_info['wc_exec_time']
+    task_sorted=np.argsort(arrival_time)
+    current_time=0
+    computed_results=[]
+
+    for task in task_sorted:
+        exec_time=wx_exec_time[task]
+        computed_results.append([task,current_time,current_time+exec_time,1])
+        current_time+=exec_time    
+
+    computed_results=np.array(computed_results,dtype=float)
+
+    return computed_results
+
 def _rate_monotonic(task_info:dict) -> np.ndarray:
     """
     Computes the scheduling base on the rate monotonic cpu scheduling algorithm
@@ -8,9 +44,10 @@ def _rate_monotonic(task_info:dict) -> np.ndarray:
     """
     computed_results=np.array([[]])
 
-    return computed_results
+    return computed_results    
 
-ALGO_MAPPING={'rate_monotonic':_rate_monotonic}
+ALGO_MAPPING={'rate_monotonic':_rate_monotonic,
+              'first_come_first_serve':_FCFS}
 
 def cpu_scheduling_compute(task_info: dict) -> np.ndarray:
     """
@@ -28,6 +65,7 @@ def cpu_scheduling_compute(task_info: dict) -> np.ndarray:
 
         {"sceduling_algo": str,
          "periods": np.array 1d,
+         "arrival_time": np.array 1d,
          "wc_exec_time": np.array 1d,
          "invoc_time": np.array 2d,
          "frequency": np.array 1d}
