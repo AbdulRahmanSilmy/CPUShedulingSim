9b6ca6e4e98d5c2439117a90319f265d7f4be236
Abdul Rahman Silmy <abdul.rahman.silmy@gmail.com>
2024-03-26 22:23:30 -0700

    added test for Rate Monotonic

--- modules/tests/test_compute.py
+++ modules/tests/test_compute.py
@@ -17,3 +17,23 @@ def test_FCFS(task_info,expected_results):
     assert np.array_equal(results,expected_results), "computed results don't match expected results" 
 
 
+@pytest.mark.parametrize("task_info, expected_results",
+                         [({'periods':np.array([8,5,10]),
+                            'wc_exec_time':np.array([1,2,2]),
+                            'end_time':15}, 
+                            np.array([[ 1,  0,  2,  1],
+                                      [ 0,  2,  3,  1],
+                                      [ 2,  3,  5,  1],
+                                      [ 1,  5,  7,  1],
+                                      [ 1,  5,  7,  1],
+                                      [ 0,  8,  9,  1],
+                                      [ 0,  8,  9,  1],
+                                      [ 1, 10, 10,  1],
+                                      [ 1, 10, 12,  1],
+                                      [ 2, 12, 14,  1],
+                                      [ 2, 12, 14,  1]]))])
+def test_RM(task_info,expected_results):
+    task_info["scheduling_algo"]='rate_monotonic'
+    results,dict_info=cpu_scheduling_compute(task_info)
+
+    assert np.array_equal(results,expected_results), "computed results don't match expected results" 
